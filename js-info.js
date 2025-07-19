74f5af2625f88adb578a868e0997201046d44045
marimar <mariayuran@yandex.by>
2025-07-19 13:36:04 +0300

    task3

---
+++ js-info.js
@@ -0,0 +1,5 @@
+alert('JavaScript был создан Бренданом Эйком');
+alert('JavaScript был разработан за 10 дней');
+alert('Язык JavaScript появился в 1995 году');
+alert('JavaScript — это не Java');
+alert('Спецификация JavaScript называется ecmascript');
