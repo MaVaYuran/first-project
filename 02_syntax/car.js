1620cbdc86ec325dada8230f27e5709d27e170c9
marimar <mariayuran@yandex.by>
2025-07-19 15:58:32 +0300

    02_syntaxTasks

---
+++ 02_syntax/car.js
@@ -0,0 +1,21 @@
+const carName = 'Ferrari';
+let maxCarSpeed = 350;
+let carOwner = 'Jean Tod';
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+maxCarSpeed = 300;
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = 330;
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = 400;
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+carOwner = 'Вася';
+console.log('У машины изменился владелец', carOwner);
+carOwner = 'Дима';
+console.log('У машины изменился владелец', carOwner);
+carOwner = 'Толя';
+console.log('У машины изменился владелец', carOwner);
---
+++ 02_syntax/index.js
@@ -0,0 +1,15 @@
+//JavaScript-код с созданием переменных
+/*Автор:
+Мария Ю*/
+
+const myName = 'Maria';
+let mySurname = 'Y';
+const myFavoriteDrink = 'Tea';
+const myFavoriteAnimal = 'horse';
+let myFavoriteProgrammingLanguage = 'JavaScript';
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
