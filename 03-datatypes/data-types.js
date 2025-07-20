4f77f0afbc3b47135bcee6aec5da700b9fee5f20
marimar <mariayuran@yandex.by>
2025-07-20 10:03:00 +0300

    03_datatypesTasks

---
+++ 03-datatypes/data-types.js
@@ -0,0 +1,19 @@
+const str = 'Maria';
+console.log('str', str, typeof str);
+const num = 32;
+console.log('num', num, typeof num);
+const isLearning = true;
+console.log('isLearning', isLearning, typeof isLearning);
+const big = 100n;
+console.log('big', big, typeof big);
+const nothing = null;
+console.log('nothing', nothing, typeof nothing);
+const undef = undefined;
+console.log('undef', undef, typeof undef);
+const symb = Symbol('symb');
+console.log('undef', undef, typeof undef);
+const adress = {
+  country: 'Belarus',
+  city: 'Minsk',
+};
+console.log('adress', adress, typeof adress);
---
+++ 03-datatypes/index.js
@@ -0,0 +1,9 @@
+let var1 = 5;
+var1 = 'dog';
+alert(var1);
+let var2 = 'horse';
+var2 = 8;
+alert(var2);
+let var3 = true;
+var3 = null;
+alert(var3);
